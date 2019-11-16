f3a6261ef494c103fc137c731631c895c14b8f61
石川祐暉 <y.sr.ku0720@gmail.com>
2019-11-16 10:01:21 +0900

    下地完了

---
+++ app/src/main/java/com/example/yuki/trustmemo/LoginActivity.kt
@@ -0,0 +1,79 @@
+package com.example.yuki.trustmemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    //Firebase
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        loginBtn.setOnClickListener {
+
+            val mail = loginMailEdit.text.toString()
+            val pass = loginPassEdit.text.toString()
+
+            if(mail.equals("")){
+                Toast.makeText(baseContext, "メールアドレスを入力してください", Toast.LENGTH_SHORT).show()
+
+            }else if(pass.equals("")) {
+                Toast.makeText(baseContext, "パスワードを入力してください", Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                auth.signInWithEmailAndPassword(mail, pass).addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(baseContext, "ログインしました。", Toast.LENGTH_SHORT).show()
+
+                            val main =  Intent(this, MainActivity::class.java)
+                            startActivity(main)
+
+                        } else {
+                            Toast.makeText(baseContext, "メールアドレスまたはパスワードが違います。", Toast.LENGTH_SHORT).show()
+
+                        }
+
+                    }
+            }
+        }
+
+        registerBtn.setOnClickListener {
+
+            val mail = loginMailEdit.text.toString()
+            val pass = loginPassEdit.text.toString()
+
+            if(mail.equals("")){
+                Toast.makeText(baseContext, "メールアドレスを入力してください", Toast.LENGTH_SHORT).show()
+
+            }else if(pass.equals("")) {
+                Toast.makeText(baseContext, "パスワードを入力してください", Toast.LENGTH_SHORT).show()
+
+            }else if(pass.length < 6) {
+                Toast.makeText(baseContext, "パスワードは6文字以上で入力してください", Toast.LENGTH_SHORT).show()
+
+            } else {
+                auth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener(this) { task ->
+
+                        if (task.isSuccessful) {
+                            Toast.makeText(baseContext, "登録しました。", Toast.LENGTH_SHORT).show()
+
+                        } else {
+                            Toast.makeText(baseContext, "登録できませんでした。", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/yuki/trustmemo/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.example.yuki.trustmemo
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    //Firebase
+    private lateinit var auth: FirebaseAuth
+    //GoogleMap
+    private lateinit var gMap: GoogleMap
+    //現在地定数
+    private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    //位置情報取得オブジェクト
+    private lateinit var lastLocation: Location
+    //更新内容取得リスナー
+    private var locationCallback : LocationCallback? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        var user = auth.currentUser
+        loginCheck(user)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    //ログインしていない状態の場合ログイン画面へ
+    private fun loginCheck(user: FirebaseUser?){
+
+        if (user == null) {
+            val login =  Intent(this, LoginActivity::class.java)
+            startActivity(login)
+        }
+    }
+
+    //パーミッション判断
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        gMap = googleMap
+        checkPermission()
+    }
+
+    private fun checkPermission(){
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            myLocationEnable()
+        } else {
+            //位置情報許可を確認
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission(){
+
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+            //既に拒否されている場合
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+        } else {
+            //初めて許可を求める時
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION -> {
+                if(permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //許可
+                    myLocationEnable()
+                } else {
+                    //拒否
+                    showToast("現在地を取得できません")
+                }
+            }
+        }
+    }
+
+    private fun myLocationEnable(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            gMap.isMyLocationEnabled = true
+            val locationRequest = LocationRequest().apply {
+                interval = 10000
+                fastestInterval = 5000
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+            locationCallback = object : LocationCallback(){
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    if(locationResult?.lastLocation != null){
+                        lastLocation = locationResult.lastLocation
+                        val currentLatLng = LatLng(lastLocation.latitude,lastLocation.longitude)
+                        gMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+                        positionView.text = "緯度：${lastLocation.latitude},経度：${lastLocation.longitude}"
+                    }
+                }
+            }
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback,null)
+        }
+    }
+
+    private fun showToast(msg: String){
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT)
+    }
+}
